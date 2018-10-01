8b8a34f702e66b5eb58be34bd2cd79b23a31e8ae
Marcus Zierke <marcuszierke@gmail.com>
2018-10-01 15:41:11 +0200

    faq for PG - not finished yet

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,4 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
-$('.faq li .question').click(function () {
-  $(this).find('.plus-minus-toggle').toggleClass('collapsed');
-  $(this).parent().toggleClass('active');
-});
 	
